de38a4e66deb656478cfffc0fca6f05827b7f11b
hanzy <hans78905@gmail.com>
2021-06-01 20:52:47 +0900

    Created a new project: Landmarks

---
+++ Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Hans Yim on 2021/06/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
