4865b4d774d3639be058434766555ac957894fc3
hanzy <hans78905@gmail.com>
2021-06-01 21:27:09 +0900

    Tutorial Section 2 done

--- Landmarks/Shared/ContentView.swift
+++ Landmarks/Shared/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, be prepared for the long journey!")
+        Text("Turtle Rock")
             .padding()
+            .font(.title)
     }
 }
 
