410cded5da5c05ddd9030f943175adc9ceed92bc
hanzy <hans78905@gmail.com>
2021-06-29 22:21:58 +0900

    Drawing Paths and Shapes: Section 1 done

---
+++ Landmarks/Shared/Views/BadgeBackground.swift
@@ -0,0 +1,17 @@
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            
+        }
+        .fill(Color.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
