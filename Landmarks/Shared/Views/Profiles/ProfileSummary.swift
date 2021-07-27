e0591229d36e07d3c5c3bff7236c96fe2653932c
hanzy <hans78905@gmail.com>
2021-07-27 22:19:25 +0900

    Working with UI Controls: Display a User Profile 2/3

--- Landmarks/Shared/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Shared/Views/Profiles/ProfileSummary.swift
@@ -2,6 +2,7 @@
 import SwiftUI
 
 struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
     var profile: Profile
     
     var body: some View {
@@ -14,6 +15,34 @@ struct ProfileSummary: View {
                 Text("Notifications: \(profile.prefersNotifications ? "On" : "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack (alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(/*@START_MENU_TOKEN@*/Angle(degrees: 90)/*@END_MENU_TOKEN@*/)
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(/*@START_MENU_TOKEN@*/0.50/*@END_MENU_TOKEN@*/)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
@@ -22,5 +51,6 @@ struct ProfileSummary: View {
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
         ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
     }
 }
