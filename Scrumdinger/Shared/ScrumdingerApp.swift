53dc752475f8ebf77a935fbfd51e85a80bfafd17
hanzy <hans78905@gmail.com>
2021-08-17 21:15:23 +0900

    iOS App Dev Tutorial Started 08/17/21

---
+++ Scrumdinger/Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Hans Yim on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
